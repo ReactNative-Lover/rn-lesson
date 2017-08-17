21ac6d84a3ac27b9f18b933cb94abc007e91f3ac
TigerChain <TigerChain>
2017-08-17 17:10:40 +0800

    '添加README.md文件'

---
+++ lesson01/01-helloworld/README.md
@@ -0,0 +1,24 @@
+### 运行步骤
+
+* 1、clonen 项目
+
+```
+ git clone https://github.com/ReactNative-Lover/rn-lesson.git
+```
+
+* 2、进入到项目目录并安装依赖
+
+```
+cd lesson01/01-helloworld/helloworld
+yarn install
+```
+
+* 3、运行项目
+
+```
+react-native run-android
+```
+
+> 查看结果：
+
+![rn-hello](./screenshot/rn-hello.gif)
--- lesson01/01-helloworld/helloworld/index.android.js
+++ lesson01/01-helloworld/helloworld/index.android.js
@@ -17,15 +17,9 @@ export default class helloworld extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Hello React
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+
       </View>
     );
   }
@@ -38,16 +32,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  welcome:{
+    fontSize:20,
+    color:'red'
+  }
 });
 
 AppRegistry.registerComponent('helloworld', () => helloworld);
