50ced39f9f0c3295ed4e35061dcc9efc5714f834
TigerChain <git_dev@163.com>
2017-08-26 18:27:05 +0800

    完成我界面

--- lesson02/01-wxdemo/wxchat/app/me/Me.js
+++ lesson02/01-wxdemo/wxchat/app/me/Me.js
@@ -7,6 +7,8 @@ import {
   StyleSheet,
 } from 'react-native';
 
+import CommonButton from '../component/CommonButton'
+
 /**
  * @auther TigerChain
  * @type {Object}
@@ -15,7 +17,68 @@ export default class Me extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>我的</Text>
+        <CommonButton
+          style={{height:70}}
+          imgSize={50}
+          marginTop={15}
+          leftText="TigerChain"
+          leftImg={require('../../imgs/avator.png')}
+          rightImg={require('../../imgs/ic_qr_code.png')}
+          onClick={()=>{
+            alert('点击')
+          }}
+        />
+
+        <CommonButton
+          marginTop={15}
+          leftText="钱包"
+          leftImg={require('../../imgs/ic_wallet.png')}
+          onClick={()=>{
+            alert('点击')
+          }}
+        />
+        <CommonButton
+          marginTop={15}
+          leftText="收藏"
+          leftImg={require('../../imgs/ic_collect.png')}
+          isShowDivider={true}
+          onClick={()=>{
+            alert('点击')
+          }}
+        />
+        <CommonButton
+          leftText="相册"
+          leftImg={require('../../imgs/ic_gallery.png')}
+          isShowDivider={true}
+          onClick={()=>{
+            alert('点击')
+          }}
+        />
+        <CommonButton
+          leftText="卡包"
+          leftImg={require('../../imgs/ic_kabao.png')}
+          isShowDivider={true}
+          onClick={()=>{
+            alert('点击')
+          }}
+        />
+        <CommonButton
+          leftText="表情"
+          leftImg={require('../../imgs/ic_emoji.png')}
+          onClick={()=>{
+            alert('点击')
+          }}
+        />
+
+        <CommonButton
+          marginTop={15}
+          leftText="设置"
+          leftImg={require('../../imgs/ic_settings.png')}
+          isShowDivider={true}
+          onClick={()=>{
+            alert('点击')
+          }}
+        />
       </View>
     );
   }
